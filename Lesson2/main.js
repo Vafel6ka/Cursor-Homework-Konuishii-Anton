21b7e8a65300c9b9fbe303959a7913553cb6df7b
Konuishii Anton <charlie_parker@ukr.net>
2020-07-19 15:06:40 +0300

    fixed a mistake in addition

--- Lesson2/main.js
+++ Lesson2/main.js
@@ -2,25 +2,28 @@ let number1 = 0;
  let number2 = 0;
  
     do {
-      number1 = +prompt ("Enter integer N");
+      number1 = +prompt ("Enter the first positive integer number");
       if (number1<0) number1 = false; //тільки "позитивні" числа
     }
     while ((!number1) && !(number1 % 1)); //тільки цілі числа, не відміна та не ""
     
     do {
-      number2 = +prompt("Enter integer M");
+      number2 = +prompt("Enter the second positive integer number");
       if (number2<0) number2 = false;
     }
     while ((!number2) && !(number2 % 1));
     
   let numbersSum = 0;
-  const isMissEven = confirm ('Are you miss even numbers? '); 
-  
-  for (let k=(Math.min(number1,number2)); k<=Math.max(number1,number2); k++) {
-    if (isMissEven) {
-       if (!(k % 2)) {numbersSum +=k}; //не сумуємо парні числа 
-    } else {
+  const isMissEven = confirm ('Are you skip even numbers? '); 
+  if (isMissEven) { //не сумуємо парні числа 
+    for (let k=(Math.min(number1,number2)); k<=Math.max(number1,number2); k++) {
+      if (!(k % 2)) continue; 
       numbersSum +=k;
+    } 
+  }   
+  else {
+    for (let k=(Math.min(number1,number2)); k<=Math.max(number1,number2); k++) {
+    numbersSum +=k;
     }
   }
 
