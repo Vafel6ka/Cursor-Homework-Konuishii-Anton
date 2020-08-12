9d91343a50d64c53e10ac0c30ec893215ea88868
Konuishii Anton <charlie_parker@ukr.net>
2020-08-12 21:35:36 +0300

    use prettier to hmw7 + fixed some small bugs

--- Lesson8/main.js
+++ Lesson8/main.js
@@ -1,23 +1,23 @@
 class Student {
-    constructor(course, university, fullName) {
-      this.university = university;
-      this.course = course;
-      this.fullName = fullName;
-      this.marksData = [4, 5, 5, 4];
-      this.isDismiss = false;
-    }
-  getInfo() {
-    return console.log("Студент " + this.course + " - го курсу " + this.university + " " +this.fullName);
+  constructor(course, university, fullName) {
+    this.university = university;
+    this.course = course;
+    this.fullName = fullName;
+    this.marksData = [4, 5, 5, 4];
+    this.isDismiss = false;
   }
-  
+  getInfo() {
+    return console.log('Студент ' + this.course + ' - го курсу ' + this.university + ' ' + this.fullName);
+  }
+
   get marks() {
     if (this.isDismiss) {
       return null;
     } else {
-    return this.marksData;
+      return this.marksData;
     }
   }
-  
+
   set marks(value) {
     if (this.isDismiss) {
       return null;
@@ -25,38 +25,37 @@ class Student {
       return this.marksData.push(value);
     }
   }
-  
+
   getAverageMark() {
     const sum = this.marksData.reduce((result, mark) => result + mark);
-    return sum/this.marksData.length
+    return sum / this.marksData.length;
   }
-  
+
   dismiss() {
-     return this.isDismiss = true;
-   }
-  
+    return (this.isDismiss = true);
+  }
+
   recover() {
-     return this.isDismiss = false;
-   }
+    return (this.isDismiss = false);
+  }
 }
 
 class BudgetStudent extends Student {
-constructor(course, university, fullName, scholarship) {
-  super (course, university, fullName);
-  this.scholarship = scholarship;
-  setInterval(() => this.getScholarship (), 30000);
-}
+  constructor(course, university, fullName, scholarship) {
+    super(course, university, fullName);
+    this.scholarship = scholarship;
+    setInterval(() => this.getScholarship(), 30000);
+  }
 
-
-  getScholarship () {
-    if ((this.getAverageMark < 5) && (this.isDismiss = false)) {
+  getScholarship() {
+    if (this.getAverageMark() <= 4 || (this.isDismiss = false)) {
       return null;
     } else {
-    return console.log("Ви отримали " + this.scholarship + " грн стипендії");
+      return console.log('Ви отримали ' + this.scholarship + ' грн стипендії');
     }
+  }
 }
-}
-const student1 = new Student("Alabama", 3, "Anton Koniushii");
+const student1 = new Student('Alabama', 3, 'Anton Koniushii');
 student1.getInfo();
 console.log(student1.marks);
 student1.marks = 4;
@@ -66,8 +65,7 @@ console.log(student1.marks);
 student1.recover();
 console.log(student1.marks);
 
-const student2 = new BudgetStudent("Alabama", 2, "Ava Gardner", 1400);
+const student2 = new BudgetStudent('Alabama', 2, 'Ava Gardner', 1400);
 student2.getInfo();
-
 console.log(student2.getAverageMark());
 student2.getScholarship();
