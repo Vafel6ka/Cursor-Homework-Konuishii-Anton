19a8e36400afe9b89cc2d7e63f79bca7094f79ac
vafel6ka <charlie_parker@ukr.net>
2020-08-14 13:47:04 +0300

    proba1

---
+++ Lesson10 /js/main.js
@@ -0,0 +1,4 @@
+const button=document.querySelector('.colorBtn1');
+button.onclick = function() {
+  color = button.style.backgroundColor;
+ if (button.style.backgroundColor==='red'){ button.style.backgroundColor='green'} else {button.style.backgroundColor='red'}}
