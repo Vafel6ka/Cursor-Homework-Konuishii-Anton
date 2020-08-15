261d50d85d07b273b7095bdc260992383d9dbd63
vafel6ka <charlie_parker@ukr.net>
2020-08-15 11:41:58 +0300

    proba2

--- Lesson10 /js/main.js
+++ Lesson10 /js/main.js
@@ -1,4 +1,5 @@
 const button=document.querySelector('.colorBtn1');
 button.onclick = function() {
-  color = button.style.backgroundColor;
- if (button.style.backgroundColor==='red'){ button.style.backgroundColor='green'} else {button.style.backgroundColor='red'}}
+ (button.style.backgroundColor==='red') ? button.style.backgroundColor='green': button.style.backgroundColor='red'}
+ 
+ //зробити два масива. в першому ссилки на музику в кількості рівній к-ті кнопок, а в другому масиві - все елементи бтн. потім почергово forEach присвоїти кожному бтн при кліку свою музику. все.
