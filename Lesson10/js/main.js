5021fbfc1603361cf570f9dc00b40aeac26f863d
Konuishii Anton <charlie_parker@ukr.net>
2020-08-15 12:29:41 +0300

    first step to do hmw10

--- Lesson10/js/main.js
+++ Lesson10/js/main.js
@@ -1,5 +1,22 @@
-const button=document.querySelector('.colorBtn1');
-button.onclick = function() {
- (button.style.backgroundColor==='red') ? button.style.backgroundColor='green': button.style.backgroundColor='red'}
- 
- //зробити два масива. в першому ссилки на музику в кількості рівній к-ті кнопок, а в другому масиві - все елементи бтн. потім почергово forEach присвоїти кожному бтн при кліку свою музику. все.
+// const button = document.querySelector('.colorBtn1');
+// button.onclick = function () {
+// 	button.style.backgroundColor === 'red'
+// 		? (button.style.backgroundColor = 'green')
+// 		: (button.style.backgroundColor = 'red');
+// };
+
+//зробити два масива. в першому ссилки на музику в кількості рівній к-ті кнопок, а в другому масиві - все елементи бтн. потім почергово forEach присвоїти кожному бтн при кліку свою музику. все.
+
+const arrBtns = [...document.querySelectorAll('.btn')];
+console.log(arrBtns);
+
+const arrColor = ['red', 'green', 'yellow', 'blue', 'black', 'cyan', 'grey', 'lightblue'];
+console.log(arrColor);
+
+arrBtns.forEach((btn, ind) => {
+	btn.onclick = function () {
+		btn.style.backgroundColor === arrColor[ind]
+			? (btn.style.backgroundColor = '#64348a')
+			: (btn.style.backgroundColor = arrColor[ind]);
+	};
+});
