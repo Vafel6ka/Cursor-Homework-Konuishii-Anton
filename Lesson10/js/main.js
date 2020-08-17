c8f1b8a89414f84ed645f2ad370410f0139350cf
vafel6ka <charlie_parker@ukr.net>
2020-08-17 18:28:07 +0300

    last ver hmw10

--- Lesson10/js/main.js
+++ Lesson10/js/main.js
@@ -1,39 +1,6 @@
-// ////////////create row and blocks
-// function generateBlock(numSqv) {
-//   function getRow() {
-//     const divRow = document.createElement('div');
-//     divRow.className = 'row';
-//     return document.body.append(divRow);
-//   }
-
-//   function getSquares() {
-//     function getSquare() {
-//       const square = document.createElement('div');
-//       square.className = 'btn';
-//       square.style.backgroundColor = '#64348a';
-//       return document.querySelector('.row').append(btn);
-//     }
-
-//     for (var i = 0; i < numSqv; i++) {
-//       getSquare();
-//     }
-//     return;
-//   }
-//   getRow();
-//   getSquares();
-//   return;
-// /////////////////////////////////
 const arrBtns = [...document.querySelectorAll('.btn')];
-console.log(arrBtns);
 
 const arrColor = ['#FBEFEF', '#F8E0E0', '#F6CECE', '#F5A9A9', '#F78181', '#FA5858', '#FE2E2E', '#FF0000'];
-console.log(arrColor);
-
-arrBtns.forEach((btn, ind) => {
-	btn.onclick = function () {
-		console.log(btn);
-	};
-});
 
 arrBtns.forEach((btn, ind) => {
 	btn.addEventListener('mousedown', function () {
@@ -41,17 +8,35 @@ arrBtns.forEach((btn, ind) => {
 	});
 });
 
-arrBtns.forEach((btn, ind) => {
+
+arrBtns.forEach((btn, ind, arr) => {
 	btn.addEventListener('mouseup', function () {
-		btn.style.backgroundColor = '#64348a';
+		let sound = document.createElement('audio');
+		  sound.setAttribute('src', `sounds/${ind}.mp3`);
+		  sound.autoplay = true;
+		  sound.load;
+		  sound.play();
+	  btn.style.backgroundColor = '#64348a';
 	});
 });
 
 arrBtns.forEach((btn, ind) => {
-	btn.addEventListener('mouseup', function () {
-		let x = document.createElement('AUDIO');
-		x.setAttribute('src', `sounds/${ind}.mp3`);
-		x.autoplay = 1;
-		document.body.appendChild(x);
-	});
+  btn.addEventListener('touchstart', function() {
+    btn.style.backgroundColor = arrColor[ind];
+  });
 });
+
+
+arrBtns.forEach((btn, ind, arr) => {
+	btn.addEventListener('touchend', function() {
+	  let sound = document.createElement('audio');
+	  sound.setAttribute('src', `sounds/${ind}.mp3`);
+	  sound.autoplay = true;
+	  sound.load;
+	  sound.play();
+	  btn.style.borderColor = "yellow";
+	 setTimeout(()=>btn.style.borderColor = 'grey', 2000);
+	  });
+	  
+});
+
