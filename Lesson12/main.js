7550aa3642301f6b1c8344835585d86b5408a28c
vafel6ka <charlie_parker@ukr.net>
2020-08-28 22:22:03 +0300

    start 12hmw

--- Lesson12/main.js
+++ Lesson12/main.js
@@ -1,20 +1,80 @@
 const BASE = 'https://swapi.dev/api/';
-function getPeoples (){
-  
-  axios.get(BASE +'films/2')
-    .then((res)=>{
-      
- return (res.data.characters).forEach(v=>axios.get(v)
-   .then((res) => {
-     console.log(res.data.name)
-    }))
-})}
-getPeoples();
+const characters = [];
+const peoples = [];
+const planets = [];
 
-function getP (){
-  axios.get(BASE +'films/2')
-    .then((res)=>{
-   console.log(res)
-})}
+function getPlanetsName() {
+  axios.get(BASE + 'planets').then((res) => {
+    res.data.results.forEach((data) => {
+      const singlePlanet = { ...data};
+      planets.push(singlePlanet);
+      showAllResults(planets)
+    });
+  });
+}
 
+//getPlanetsName();
+
+function getP() {
+  axios.get(BASE + 'planets').then((res) => console.log(res))
+}
 getP();
+
+function getChars2() {
+  axios.get(BASE + 'films/2')
+  .then((res) => {
+    res.data.characters.forEach((data, ind) => {
+      axios.get(data)
+      .then((character) => {
+        const singlePeople = { ...character.data };
+        characters.push(singlePeople);
+      showResults(characters);
+      })});
+    });
+}
+
+function getPeople2() {
+  axios.get(BASE + "people/2").then((res) => {
+    const singlePeople = { ...res.data };
+    peoples.push(singlePeople);
+    showResults(peoples);
+  });
+}
+
+//function showResults(data) {
+ // console.log(data);
+//}
+
+function showResults(users = {}) {
+  const container = document.querySelector('.wrapper');
+  container.innerHTML = '';
+
+  users.forEach(user => {
+    const userElement = document.createElement('div');
+    userElement.className = 'user';
+    userElement.innerHTML = `
+      <h3>${user.name}</h3>
+      <h4>${user.birth_year}</h4>
+      <h4>${user.gender}</h4>
+    `;
+    container.append(userElement);
+  });
+}
+
+function showAllResults(users = {}) {
+  const container = document.querySelector('.wrapper');
+  container.innerHTML = '';
+
+  users.forEach(user => {
+    const userElement = document.createElement('div');
+    userElement.className = 'user';
+    for (let key in user) {
+      const userElement2 = document.createElement('div');
+    userElement2.innerHTML = `
+      <h3>${key} : ${user[key]}</h3>
+    `;
+    
+    container.append(userElement2);}
+    container.append(userElement)
+  });
+}
