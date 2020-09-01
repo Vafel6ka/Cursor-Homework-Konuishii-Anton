d19ce74c56763594c1f156d34f009cbb39581e3a
vafel6ka <charlie_parker@ukr.net>
2020-09-01 11:35:40 +0300

    start hmw13

--- Lesson13/js/main.js
+++ Lesson13/js/main.js
@@ -1,3 +1,6 @@
+const id=[];
+let count = 0;
+
 function* gen () {
   let i = 0;
   while (true) {
@@ -6,8 +9,42 @@ function* gen () {
   }
 }
 
+function* changeFont () {
+  yield 12
+  while (true) {
+    yield 12
+    yield 14
+    yield 16
+  }
+}
+
+function getId (){
+  id.push(generator.next().value);
+  count++
+  return id[count-1];
+}
+
+function showAllId() {
+  console.log(id)
+}
+
 const generator = gen();
-console.log(generator.next().value);
-console.log(generator.next().value);
-console.log(generator.next().value);
-console.log(generator.next().value);
+const changer = changeFont();
+
+function showId (){
+const el = document.querySelector('.idd');
+el.innerHTML = `
+      <p>Id : ${getId()}</p>`
+      }
+      
+function fontUp (){
+  changer.next().value;
+  const text = document.querySelector(".test");
+  text.style.fontSize = changer.next().value
+  text.innerHTML = ``;
+  text.innerHTML = `
+        <p>texttext</p>`
+  }
+  
+  
+  
